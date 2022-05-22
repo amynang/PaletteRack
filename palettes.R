303317d03e4c57c6875dcbec83dd1d9798ba8ba4
Angelos Amyntas <61004068+amynang@users.noreply.github.com>
2022-05-22 20:10:29 +0200

    Update palettes.R

--- palettes.R
+++ palettes.R
@@ -1,4 +1,5 @@
 library(RImagePalette)
+set.seed(42)
 
 https://uploads6.wikiart.org/00135/images/toshusai-sharaku/sharaku-1794-soga-gor-and-gosho-gor-maru-adachi-reprint.jpg!Large.jpg
 https://www.sott.net/image/s16/323691/full/ancient_greek_mosaic_excavatio.jpg
